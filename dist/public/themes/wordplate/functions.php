0703db416c3523a77d8968246af603ab09b7cdc5
Willy Brauner <mail@willybrauner.com>
2018-11-04 14:11:36 +0100

    install wordplate

---
+++ dist/public/themes/wordplate/functions.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+// Register plugin helpers.
+require template_path('includes/plugins/plate.php');
+
+// Set theme defaults.
+add_action('after_setup_theme', function () {
+    // Disable the admin toolbar.
+    show_admin_bar(false);
+
+    // Add post thumbnails support.
+    add_theme_support('post-thumbnails');
+
+    // Add title tag theme support.
+    add_theme_support('title-tag');
+
+    // Add HTML5 theme support.
+    add_theme_support('html5', [
+        'caption',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'search-form',
+        'widgets',
+    ]);
+
+    // Register navigation menus.
+    register_nav_menus([
+        'navigation' => __('Navigation', 'wordplate'),
+    ]);
+});
+
+// Enqueue and register scripts the right way.
+add_action('wp_enqueue_scripts', function () {
+    wp_deregister_script('jquery');
+
+    // wp_enqueue_style('wordplate', mix('styles/app.css'));
+
+    // wp_register_script('wordplate', mix('scripts/app.js'), '', '', true);
+    // wp_enqueue_script('wordplate');
+});
+
+// Remove JPEG compression.
+add_filter('jpeg_quality', function () {
+    return 100;
+}, 10, 2);
