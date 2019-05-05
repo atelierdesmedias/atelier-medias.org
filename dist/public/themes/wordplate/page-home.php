a7d8b02791432976b49f1133bd6a043fca0177bd
willybrauner <mail@willybrauner.com>
2019-05-05 21:41:39 +0200

    feat: dev back homepage/footer + adaptation templates

--- dist/public/themes/wordplate/page-home.php
+++ dist/public/themes/wordplate/page-home.php
@@ -3,6 +3,17 @@
  * Home Page
  */
 
-$context = Timber::get_context();
-Timber::render('templates/pages/HomePage.twig', $context);
+// load current post
+$post = new Timber\Post();
+$data = Timber::get_context();
+$data['post'] = $post;
+
+// load contact page by id
+$contact_page_id = 11;
+$contact = new TimberPost( $contact_page_id );
+$data['contact'] = $contact;
+
+//dump($data);
+
+Timber::render('templates/pages/HomePage.twig', $data);
 
