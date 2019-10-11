0fd3a98afee3ed9fc6dda6647982a6b08816a736
Alexandre FREARD <u54137392@holyowlstudio.com>
2019-10-11 10:27:10 +0200

    some modification for windows users install

--- properties/alex.properties.js
+++ properties/alex.properties.js
@@ -1,42 +1,39 @@
-/**
- * Set alex properties
- */
-module.exports = {
 
-    // assets are served on this url
-    url: 'http://localhost:3000/',
-    port: 3000,
+            /**
+             * Set alex properties
+            */
+            module.exports = {
+            
+                /**
+                 * Application base.
+                 *
+                 * - Set path from domain name to application. Starting and ending with slash.
+                 * - ex :
+                 *        If application is installed here : http://domain.com/my-sub-folder/my-app/
+                 *        Base should be : "/my-sub-folder/my-app/"
+                 * - ex :
+                 *        If application is installed here : http://domain.com/
+                 *        Base should be : "/"
+                 */
+                 
+                base: '/',
+                
+                dotEnv: Object.assign(
+                {},
+                require('./default.properties').dotEnv,
+                {
+                    // Activer le mode debug
+                    WP_ENV: "local",
+                    WP_DEBUG: "true",
+        
+                    // Cibler le chemin complet vers point d'entrée wordpress (dist/public)
+                    // Pas de slash à la fin
+                    WP_URL: "http://localhost/atelier-medias.org/dist/public",
 
-    /**
-     * Application base.
-     *
-     * - Set path from domain name to application. Starting and ending with slash.
-     * - ex :
-     *        If application is installed here : http://domain.com/my-sub-folder/my-app/
-     *        Base should be : "/my-sub-folder/my-app/"
-     * - ex :
-     *        If application is installed here : http://domain.com/
-     *        Base should be : "/"
-     */
-
-    base: '/',
-
-    dotEnv: Object.assign(
-        {},
-        require('./default.properties').dotEnv,
-        {
-            // Activer le mode debug
-            WP_ENV: "local",
-            WP_DEBUG: "true",
-
-            // Cibler le chemin complet vers point d'entrée wordpress (dist/public)
-            // Pas de slash à la fin
-            WP_URL: "http://localhost/dist/public",
-
-            // Meme URL que WP url avec "/wordpress" à la fin
-            WP_SITEURL: "http://localhost/dist/public/wordpress",
-
-            // Le nom de la base de données
-            DB_NAME: "admwp",
-        })
-};
+                    // Meme URL que WP url avec "/wordpress" à la fin
+                    WP_SITEURL:"http://localhost/atelier-medias.org/dist/public/wordpress",
+        
+                    // Le nom de la base de données
+                    DB_NAME: "admwp",
+                })
+            };
