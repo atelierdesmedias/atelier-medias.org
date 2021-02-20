c9597e5f8df7d036abf5aefa85d2df253ee20753
Martin Delille <martin@delille.org>
2021-02-20 22:00:43 +0100

    Run scaffhold for CoworkersPage

---
+++ src/pages/coworkersPage/CoworkersPage.ts
@@ -0,0 +1,67 @@
+/**
+ * this is "CoworkersPage" TS export class
+ * follow workflow instructions
+ */
+
+import {DOMView} from "../../helpers/solidify-lib/core/DOMView"
+import './CoworkersPage.scss'
+
+// ----------------------------------------------------------------------------- START EXPORT CLASS
+
+export class CoworkersPage extends DOMView
+{
+
+    // ------------------------------------------------------------------------- INIT
+
+    /**
+     * Target our root if not already defined via constructor params
+     */
+    protected targetRoot()
+    {
+
+    }
+
+    /**
+     * prepare nodes
+     */
+    protected prepareNodes()
+    {
+
+    }
+
+    /**
+     * prepare dependencies
+     */
+    protected prepareDependencies()
+    {
+
+    }
+
+    /**
+     *  init components
+     */
+    protected initComponents()
+    {
+
+    }
+
+    /**
+     * prepare events
+     */
+    protected prepareEvents()
+    {
+
+    }
+
+    /**
+     * after Init
+     */
+    protected afterInit()
+    {
+
+    }
+
+    // ------------------------------------------------------------------------- HANDLERS
+
+}
+
